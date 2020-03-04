56d22b29206f32d538e7543988bdf9d0dbe044a3
kanoop640 <kanoop640@gmail.com>
2020-03-04 17:08:53 +0530

    [Anoop]Added Load data form csv file

--- CensusAnalyzer/CSVStateCensus.cs
+++ CensusAnalyzer/CSVStateCensus.cs
@@ -1,11 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿// <copyright file="CSVStateCensus.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Anoop kumar"/>
+// -----------------------------------------------------------------
 namespace CensusAnalyzer
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.IO;
     class CSVStateCensus
     {
-
+        /// <summary>
+        /// CSVs the data count.
+        /// which return the number of data which it reads form 
+        /// StateCensusData.csv file
+        /// </summary>
+        /// <returns></returns>
+        public int LoadCSVStateData()
+        {
+            int count = 0;
+            string[] str = File.ReadAllLines(@"D:\Anoop_kumar\CensusAnalyzer\CensusAnalyzer\File\StateCensusData.csv");
+            IEnumerable<string> ele = str;
+            foreach (string line in ele)
+            {
+                count++;
+            }
+            return count;
+        }
     }
 }
--- CensusAnalyzer/StateCensusAnalyzer.cs
+++ CensusAnalyzer/StateCensusAnalyzer.cs
@@ -1,10 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿// <copyright file="StateCensusAnalyzer.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Anoop kumar"/>
+// -----------------------------------------------------------------
 namespace CensusAnalyzer
 {
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Text;
     class StateCensusAnalyzer
     {
+        public int LoadStateData()
+        {
+            var data = File.ReadAllLines(@"D:\Anoop_kumar\CensusAnalyzer\CensusAnalyzer\File\StateCensusData.csv");
+            return data.Length;
+        }
     }
 }
