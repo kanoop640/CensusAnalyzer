07fc4370b1169e2502b8a61e47cea23a2c8067d1
kanoop640 <kanoop640@gmail.com>
2020-03-16 15:08:57 +0530

    [Anoop]Refactor UseCase 7

--- CensusAnalyzer/Program.cs
+++ CensusAnalyzer/Program.cs
@@ -12,14 +12,15 @@ namespace CensusAnalyzer
             string csvStateCensusJsonPath = @"D:\Anoop_kumar\CensusAnalyzer\CensusAnalyzer\File\StateCensusData.json";
             string sortJsonCensusByState = @"D:\Anoop_kumar\CensusAnalyzer\CensusAnalyzer\File\sortStateCensusDataByState.json";
             string sortJsonCensusByPopulation = @"D:\Anoop_kumar\CensusAnalyzer\CensusAnalyzer\File\sortStateCensusDataByPopulation.json";
-            string sortJsonCensusByPopulationDensity = @"D:\Anoop_kumar\CensusAnalyzer\CensusAnalyzer\File\sortStateCensusDataByPopulationvDensity.json";
+            string sortJsonCensusByPopulationDensity = @"D:\Anoop_kumar\CensusAnalyzer\CensusAnalyzer\File\sortStateCensusDataByArea.json";
+            string sortJsonCensusByArea = @"D:\Anoop_kumar\CensusAnalyzer\CensusAnalyzer\File\sortStateCensusDataByArea.json";
             string sortJsonCode = @"D:\Anoop_kumar\CensusAnalyzer\CensusAnalyzer\File\sortStateCode.json";
             string csvStateCodePath = @"D:\Anoop_kumar\CensusAnalyzer\CensusAnalyzer\File\StateCode.csv";
             string csvStateCodeJsonPath = @"D:\Anoop_kumar\CensusAnalyzer\CensusAnalyzer\File\StateCode.json";
             Console.WriteLine("Welcome to CensusAnalyzer");
             CSVStates csvStateCodeObject = new CSVStates();
             csvStateCodeObject.CSVStateCodeJsonDataLoad(csvStateCodePath, csvStateCodeJsonPath, sortJsonCode, "State Name");
-            csvStateCodeObject.CSVStateCodeJsonDataLoadForNum(csvStateCensusPath, csvStateCensusJsonPath, sortJsonCensusByPopulationDensity, "DensityPerSqKm");
+            csvStateCodeObject.CSVStateCodeJsonDataLoadForNum(csvStateCensusPath, csvStateCensusJsonPath, sortJsonCensusByArea, "AreaInSqKm");
         }
     }
 }
